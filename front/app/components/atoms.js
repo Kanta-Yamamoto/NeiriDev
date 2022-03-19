dd0b542eade938cc3aeb77348032097206f0238a
Kanta-Yamamoto <84559990+Kanta-Yamamoto@users.noreply.github.com>
2022-03-19 18:36:59 +0900

    Initialize repository

---
+++ front/app/components/atoms.js
@@ -0,0 +1,21 @@
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
+
+//count
+export const countState = atom({
+  key: "count",
+  default: 0,
+  effects_UNSTABLE: [persistAtom]
+});
+
+//user
+export const userState = atom({
+  key: "user",
+  default: {
+    name: "hoge",
+    age: 11
+  },
+  effects_UNSTABLE: [persistAtom] //追加
+});
